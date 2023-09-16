952a65f555b873897f1dc22bb5f67f220360dd27
Mitali <mitali.kawde11@gmail.com>
2023-09-16 18:37:21 +0530

    add route for about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
   root to: 'products#index'
+
+  get '/about', to: 'about#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
