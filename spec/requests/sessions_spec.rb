bbb0ecf1bfbe8fbc9ca3d41fbfade29fdc5f66a2
Mitali <mitali.kawde11@gmail.com>
2023-09-17 01:38:23 +0530

    create sessions controller

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /new" do
+    it "returns http success" do
+      get "/sessions/new"
+      expect(response).to have_http_status(:success)
+    end
   end
+
 end
