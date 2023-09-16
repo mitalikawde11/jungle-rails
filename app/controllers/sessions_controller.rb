3406f5d0b11af972f5b7f86d1eb2a88ad0f7e44e
Mitali <mitali.kawde11@gmail.com>
2023-09-17 02:00:48 +0530

    create actions for login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,25 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
   def create
-    # If user is successfully authenticated
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
-      # Save user id as cookie so they stay logged in
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to :root
-    else 
-      render :new
+      redirect_to '/'
+    else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
     end
+
   end
 
   def destroy
-    # Remove the cookie
     session[:user_id] = nil
-    redirect_to :root
+    redirect_to '/login'
   end
 
 end
