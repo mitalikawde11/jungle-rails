97bc996477deeb4c2250b92a406509b437eaec86
Mitali <mitali.kawde11@gmail.com>
2023-09-16 17:06:48 +0530

    not implemented, trying

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,11 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+   # Login form, logging in and logging out
+  #  get '/login' => 'sessions#new'
+  #  post '/login' => 'sessions#create'
+  #  get '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
